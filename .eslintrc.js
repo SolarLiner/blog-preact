c1553421a748a7fdb15efe3657a1bf234e2db84c
Nathan Graule <solarliner@gmail.com>
2020-03-14 14:50:10 +0100

    Chore: Add TypeScript, ESLint and Prettier to project

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+const isDev = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    debugLevel: true,
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"]
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier"
+  ],
+  rules: { "no-console": isDev ? "warn" : "error" }
+};
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,21 +1,20 @@
-import { h, Component } from 'preact';
-import { Router } from 'preact-router';
-import { Provider } from '@preact/prerender-data-provider';
-import Header from './header';
-
+import { Component, h } from "preact";
+import { Router } from "preact-router";
+import { Provider } from "@preact/prerender-data-provider";
+import Header from "./header";
 // Code-splitting is automated for routes
-import Home from '../routes/home';
-import Blogs from '../routes/blogs';
-import Blog from '../routes/blog';
-import Contact from '../routes/contact';
-import ContactSuccess from '../routes/contact-success';
-import NotFoundPage from '../routes/notfound';
+import Home from "../routes/home";
+import Blogs from "../routes/blogs";
+import Blog from "../routes/blog";
+import Contact from "../routes/contact";
+import ContactSuccess from "../routes/contact-success";
+import NotFoundPage from "../routes/notfound";
 
 export default class App extends Component {
 
 	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
+	 *  @param {Object} event    "change" event from [preact-router](http://git.io/preact-router)
+	 *  @param {string} event.url  The newly routed URL
 	 */
 	handleRoute = e => {
 		this.currentUrl = e.url;
