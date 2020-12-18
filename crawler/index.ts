1f1c22d969aadf666e811a79eee525de82ca53d2
Nathan Graule <solarliner@gmail.com>
2020-12-18 12:07:07 +0100

    refactor: better error printing on crawler error

--- crawler/index.ts
+++ crawler/index.ts
@@ -3,18 +3,22 @@ import readingTime from "reading-time";
 import { flow } from "fp-ts/lib/function";
 import { FrontmatterDerived, FrontmatterRaw, FrontmatterT } from "./types";
 import { Either, isLeft } from "fp-ts/lib/Either";
+import { Errors } from "io-ts";
 
 export default function crawl(root: string): Promise<Array<MarkdownNode<FrontmatterRaw & FrontmatterDerived>>> {
   // eslint-disable-next-line @typescript-eslint/unbound-method
-  return crawlMarkdown(root, flow(FrontmatterT.decode, unwrap)).then(mds => mds.map(addDerivedMetadata));
+  return crawlMarkdown(root, flow(FrontmatterT.decode, unwrapDecode)).then(mds => mds.map(addDerivedMetadata));
 }
 
-function unwrap<T, E>(either: Either<E, T>): T {
-  if (isLeft(either)) {
-    throw new Error(JSON.stringify(either.left, null, 2));
-  }
-  return either.right;
-}
+const unwrap = <E>(printer: (err: E) => string) => <T>(either: Either<E, T>): T => {
+  if (isLeft(either)) throw new Error("Error: " + printer(either.left));
+  else return either.right;
+};
+
+const unwrapDecode = unwrap((errors: Errors) => errors.map(err => {
+  const ctx = err.context[err.context.length - 1];
+  return `${ctx.key} should be ${ctx.type.name} but was ${ctx.actual}`;
+}).join(", "));
 
 function addDerivedMetadata(node: MarkdownNode<FrontmatterRaw>): MarkdownNode<FrontmatterRaw & FrontmatterDerived> {
   const { words, minutes } = readingTime(node.contents);
